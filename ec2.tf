41e685dd7c70bbe4af6e0d7d323e87039ff2ca89
crem72 <curtisreynoldsphoto@gmail.com>
2025-11-19 15:47:16 +0000

    tf ec2 with sec group done

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,9 @@ resource "aws_instance" "example" {
   instance_type = var.machine_size
   key_name = var.private_key_name
 
+  # adding security group to ec2 vm
+  vpc_security_group_ids = [aws_security_group.curt_sec-groups.id]
+
   tags = {
     Name = var.vm_name
   }
--- security-groups.tf
+++ security-groups.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "curt_sec-groups" {
   vpc_id      = data.aws_vpc.curt-existing-vpc.id
 
   tags = {
-    Name = "curt_firewall_rules"
+    Name = "curt_firewall_rules_final"
   }
 }
 
@@ -14,4 +14,23 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.curt_sec-groups.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+# allow 22 port from everywhere
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.curt_sec-groups.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+# allow 80 port from everywhere
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4_http" {
+  security_group_id = aws_security_group.curt_sec-groups.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
 }
